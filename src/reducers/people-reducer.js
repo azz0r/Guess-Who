9fd77e8d2026f6a812a0111e93bc41b84c8d9440
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-08-14 10:31:43 +0100

    Add wrestlers to default state

--- src/reducers/people-reducer.js
+++ src/reducers/people-reducer.js
@@ -1,6 +1,7 @@
 import * as types from '../actions/action-types';
+import People from '../data/wrestlers.json';
 
-export default (state = [], action) => {
+export default (state = People, action) => {
   switch (action.type) {
     case types.ADD_PERSON:
       return [...state, Object.assign({}, action.person)];
