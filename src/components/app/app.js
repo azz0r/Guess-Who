f76aaedb350afd233b39de33ccc9ebfc22b034f9
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-08-23 18:57:41 +0100

    Container fluid and useless links removed

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,17 +5,10 @@ import PeopleContainer from '../who/who';
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container-fluid">
         <div className="header clearfix">
-          <nav>
-            <ul className="nav nav-pills pull-right">
-              <li role="presentation"><a href="/">Reset Game</a></li>
-              <li role="presentation"><a href="#">Github</a></li>
-            </ul>
-          </nav>
-          <h3 className="text-muted">Guess Who</h3>
+          <h3>Guess Who</h3>
         </div>
-
         <PeopleContainer />
       </div>
     );
