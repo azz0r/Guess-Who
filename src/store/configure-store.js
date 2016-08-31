3ffde61adf83217b46e7a778b69598f0ed93958b
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-08-31 17:43:12 +0100

    persistState added, but disabled by default Added redux logger

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,9 +1,14 @@
 import reducers from '../reducers';
+// import persistState from 'redux-localstorage';
 import { batch, batching } from 'redux-batch-middleware';
 import { createStore, applyMiddleware, compose } from 'redux';
+import createLogger from 'redux-logger';
+
+const logger = createLogger();
 
 const storeEnhancer = compose(
-  applyMiddleware(batch),
+  applyMiddleware(batch, logger),
+  // persistState(),
   typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
     ? window.devToolsExtension()
     : f => f
