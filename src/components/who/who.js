5ef1044c7bd09b24544cc70ed5f9d276b3576b15
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-09-25 11:59:00 +0100

    Switch the presentation into a separate component

--- src/components/who/who.js
+++ src/components/who/who.js
@@ -1,6 +1,5 @@
-import React, { PropTypes, Component } from 'react'
-import { Questions } from './questions'
-import { People, ChooseAPerson, PersonChosen } from './people'
+import React from 'react'
+import Presentation from './who.presentation'
 import * as PlayersActions from '../../actions/players'
 import * as QuestionActions from '../../actions/questions'
 import * as ModalActions from '../../actions/modal'
@@ -11,13 +10,14 @@ const playerIds = {
   human: 0,
   bot: 1
 }
-class Who extends Component {
+
+class Who extends React.Component {
 
   static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    players: PropTypes.array.isRequired,
-    questions: PropTypes.array.isRequired,
-    modal: PropTypes.array.isRequired,
+    dispatch: React.PropTypes.func.isRequired,
+    players: React.PropTypes.array.isRequired,
+    questions: React.PropTypes.array.isRequired,
+    modal: React.PropTypes.array.isRequired,
   }
 
   componentDidMount() {
@@ -124,126 +124,30 @@ class Who extends Component {
   }
 
   _getWinner() {
-    let weHaveAWinner = false,
-      winnerId = false;
+    let hasWinner = false,
+      id = false;
     this.props.players.forEach((player, key) => {
       if (player.people.filter((person) => !person.chosen).length === 1) {
-        weHaveAWinner = true
-        winnerId = key
+        hasWinner = true
+        id = key
       }
     })
     return {
-      weHaveAWinner,
-      winnerId,
+      hasWinner,
+      id,
     }
   }
 
   render() {
-    let { weHaveAWinner, winnerId } = this._getWinner()
     return (
-      <div className="who-container">
-        <Choose>
-          <When condition={this.props.modal.open && !weHaveAWinner}>
-            <div className="row text-center">
-              <h3>Opponents turn!</h3>
-              <h3>
-                {this.props.modal.question}
-              </h3>
-              <div className="text-center">
-                <Choose>
-                  <When condition={this.props.modal.answer === true}>
-                    <img src="/static/imgs/yes.png" role="presentation" />
-                  </When>
-                  <Otherwise>
-                    <img src="/static/imgs/no.gif" role="presentation" />
-                  </Otherwise>
-                </Choose>
-              </div>
-              <br />
-              <div>
-                <a
-                  className="btn"
-                  onClick={this.onCloseModal}>
-                  Your Turn
-                </a>
-              </div>
-            </div>
-            <div className="row">
-              <div className="sidebar col-xs-12 col-md-3 col-sm-3 col-lg-3 text-center">
-                <h2>Your chosen character</h2>
-                <PersonChosen
-                  person={this.props.players[playerIds.human].chosenPerson}
-                />
-              </div>
-              <div className="col-xs-9 zoom-out-5">
-                <div className="board-wrapper">
-                  <People
-                    people={this.props.players[playerIds.bot].people}
-                    hidePersonsFace
-                  />
-                </div>
-              </div>
-            </div>
-          </When>
-          <When condition={weHaveAWinner}>
-            <div className="winner col-xs-12 text-center">
-              {this.props.players[winnerId].name} won the game by narrowing it down to...
-              <PersonChosen
-                person={this.props.players[(winnerId === 0 ? 1 : 0)].chosenPerson}
-              />
-              <p>
-                <a
-                  href="#"
-                  className="btn cursor-pointer"
-                  onKeyPress={this.onResetGame}
-                  onClick={this.onResetGame}>
-                  Play again?
-                </a>
-              </p>
-            </div>
-          </When>
-          <Otherwise>
-            <div className="row human-board">
-              <div className="sidebar col-xs-12 col-md-3 col-sm-3 col-lg-3">
-                  <Choose>
-                    <When condition={this.props.players[playerIds.human].chosenPerson}>
-                      <PersonChosen
-                        person={this.props.players[playerIds.human].chosenPerson}
-                        hidePersonsFace
-                      />
-                    </When>
-                    <Otherwise>
-                      <ChooseAPerson person={this.props.players[playerIds.human].chosenPerson} />
-                    </Otherwise>
-                  </Choose>
-                  <Questions
-                    active={this.props.players[playerIds.human].currentTurn && this.props.players[playerIds.human].chosenPerson}
-                    limit={5}
-                    shuffle
-                    onQuestionChosen={this.onQuestionChosen}
-                    questions={this.props.questions[playerIds.human]}
-                  />
-                <div>
-                  <a
-                    href="#"
-                    className="btn cursor-pointer"
-                    onKeyPress={this.onResetGame}
-                    onClick={this.onResetGame}>Reset Game
-                  </a>
-                </div>
-              </div>
-              <div className="col-xs-9">
-                <div className="board-wrapper">
-                  <People
-                    people={this.props.players[playerIds.human].people}
-                    onPersonClicked={this.onPersonClicked}
-                  />
-                </div>
-              </div>
-            </div>
-          </Otherwise>
-        </Choose>
-      </div>
+      <Presentation
+        {...this.props}
+        winner={this._getWinner()}
+        onResetGame={this.onResetGame}
+        onCloseModal={this.onCloseModal}
+        onPersonClicked={this.onPersonClicked}
+        onQuestionChosen={this.onQuestionChosen}
+       />
     )
   }
 }
